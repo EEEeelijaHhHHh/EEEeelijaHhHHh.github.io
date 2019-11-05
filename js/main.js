206c5b50d6209d5685e699523304d8655bd1480f
EEEeelijaHhHHh <elijahs.adjutovs@gmail.com>
2019-11-05 12:16:57 +0200

    Added hamburger && Adaptive works, header, nav, contact

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// navigation hamburger
+(function () {
+  const headerContent = document.querySelector('.header-content')
+  const nav = document.querySelector('.nav');
+  const menu = document.querySelector('.nav-menu');
+  const hamburger = document.querySelector('.nav-hamburger');
+
+  const links = document.querySelectorAll('.nav-menu__link');
+
+  hamburger.addEventListener('click', function () {
+    toggleMenu();
+  });
+
+  links.forEach(link => link.addEventListener('click', () => {
+    toggleMenu();
+  }));
+
+  function toggleMenu() {
+    headerContent.classList.toggle('header-content_active');
+    nav.classList.toggle('nav_active');
+    menu.classList.toggle('nav-menu_active');
+    hamburger.classList.toggle('nav-hamburger_active');
+  }
+}());
